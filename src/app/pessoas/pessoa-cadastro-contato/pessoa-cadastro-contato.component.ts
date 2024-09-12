d61c740d4c2449690725bb45a290a6ae799e7ada
marcofavero3 <marcofavero3@live.com>
2024-09-12 16:34:13 -0300

    update front final v1

---
+++ src/app/pessoas/pessoa-cadastro-contato/pessoa-cadastro-contato.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoa-cadastro-contato',
+  standalone: true,
+  imports: [],
+  templateUrl: './pessoa-cadastro-contato.component.html',
+  styleUrl: './pessoa-cadastro-contato.component.css'
+})
+export class PessoaCadastroContatoComponent {
+
+}
---
+++ src/app/relatorios/relatorio-lancamentos/relatorio-lancamentos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-relatorio-lancamentos',
+  standalone: true,
+  imports: [],
+  templateUrl: './relatorio-lancamentos.component.html',
+  styleUrl: './relatorio-lancamentos.component.css'
+})
+export class RelatorioLancamentosComponent {
+
+}
